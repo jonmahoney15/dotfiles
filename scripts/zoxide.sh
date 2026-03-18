5df707f07180a967bdf45777810953189193aaba
Jon Mahoney <jmahoney@blueorigin.com>
2026-03-18 14:01:02 -0700

    Update Tmux to better feel
    Tmux didnt match general feel.
    
    Added Catpuccin theme to tmux
    
    removed old scripts

--- scripts/zoxide.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
--- scripts/zsh.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-apt install zsh
-
-chsh -s /usr/bin/zsh vagrant
-
-awk -F: -v user="vagrant" '$1 == user {print $NF}' /etc/passwd
